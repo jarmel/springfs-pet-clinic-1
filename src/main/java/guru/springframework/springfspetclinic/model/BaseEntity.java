6309f38e8f66c6c5fe1aca13020f86e7e2a9aac9
Tom Thompson <tomthompsonrph2@gmail.com>
2025-12-06 17:12:42 -0700

    Update BaseEntity.java

--- src/main/java/guru/springframework/springfspetclinic/model/BaseEntity.java
+++ src/main/java/guru/springframework/springfspetclinic/model/BaseEntity.java
@@ -7,12 +7,23 @@ import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
 
 @MappedSuperclass
+<<<<<<< Updated upstream
 public class BaseEntity {
+=======
+public abstract class BaseEntity {
+>>>>>>> Stashed changes
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     protected Integer id;
 
+<<<<<<< Updated upstream
+=======
+    public Integer getId() {
+        return id;
+    }
+
+>>>>>>> Stashed changes
     public void setId(Integer id) {
         this.id = id;
     }
@@ -21,4 +32,8 @@ public class BaseEntity {
     public boolean isNew() {
         return this.id == null;
     }
+<<<<<<< Updated upstream
 }
+=======
+}
+>>>>>>> Stashed changes
